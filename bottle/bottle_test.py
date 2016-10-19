735af79f744c052eef72151bf13802928d45b556
Lei Zhang <761141362@qq.com>
2016-10-19 11:24:26 +0800

    create bottle

---
+++ bottle/bottle_test.py
@@ -0,0 +1,10 @@
+import requests
+
+resp = requests.get('http://localhost:9999/echo/zhang')
+if resp.status_code == 200 and \
+  resp.text == 'Say hello to my little friend: zhang!':
+    print('It worked! That almost never happens!')
+else:
+    print('Argh, got this:', resp.text)
+    
+
